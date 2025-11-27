c641b22b9a1d62b15e6937062199cb35060714fd
ayushbitsom <ayushkumar.casecomp@gmail.com>
2025-11-27 14:52:27 +0530

    Update config.ts

--- config.ts
+++ config.ts
@@ -30,10 +30,10 @@ function getDateAndTime(): string {
 
 export const DATE_AND_TIME = getDateAndTime();
 
-export const AI_NAME = "MyAI3";
-export const OWNER_NAME = "Ayush Kumar";
+export const AI_NAME = "DigiSarthi";
+export const OWNER_NAME = "Ayush Kumar & Rishika Agrawal";
 
-export const WELCOME_MESSAGE = `Hello! I'm ${AI_NAME}, an AI assistant created by ${OWNER_NAME}.`
+export const WELCOME_MESSAGE = `Hello! I am Digisarthi, your guide on the journey of digital growth. How can I help your business take off today?`
 
 export const CLEAR_CHAT_TEXT = "New";
 
